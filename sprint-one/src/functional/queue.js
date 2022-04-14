6167e38eea59d200c56c91cca81e0a47c0135efd
Joy <parkerjn90@gmail.com>
2022-04-14 15:30:42 -0400

    solve functional instatiation/queue first attempt: returning undefined

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,17 +5,25 @@ var Queue = function () {
   var storage = {};
 
   var objLength = 0;
-
+  var firstKey = 0;
   // Implement the methods below
 
   someInstance.enqueue = function (value) {
-    objLength += 1;
+    // define new key in {storage} as objLength, value = input value, increment objLength
+    storage[objLength++] = value;
   };
 
   someInstance.dequeue = function () {
+    // create var to store firstKey value
+    var firstValue = storage[firstKey];
+    // remove first property (key = firstKey)
+    delete (storage[firstKey]);
+    // increment firstKey variable by 1
     if (objLength > 0) {
       objLength -= 1;
     }
+    //return var storing firstKey value
+    return firstValue;
   };
 
   someInstance.size = function () {
