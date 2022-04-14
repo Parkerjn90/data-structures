f7b1426a15044de488d9d760d0a7a6c115f9d4eb
Joy <parkerjn90@gmail.com>
2022-04-13 22:35:44 -0400

    save preliminary tests for functional instatiation patters for both stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,25 @@
-var Queue = function() {
+var Queue = function () {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var objLength = 0;
+
   // Implement the methods below
 
-  someInstance.enqueue = function(value) {
+  someInstance.enqueue = function (value) {
+    objLength += 1;
   };
 
-  someInstance.dequeue = function() {
+  someInstance.dequeue = function () {
+    if (objLength > 0) {
+      objLength -= 1;
+    }
   };
 
-  someInstance.size = function() {
+  someInstance.size = function () {
+    return objLength;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,29 @@
 var Stack = function() {
-  var someInstance = {};
+  var someInstance = {
+
+  };
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {
+
+  };
+
+  var objectLength = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    objectLength += 1;
   };
 
   someInstance.pop = function() {
+    if (objectLength > 0) {
+      objectLength -= 1;
+    }
+
   };
 
   someInstance.size = function() {
+    return objectLength;
   };
 
   return someInstance;
