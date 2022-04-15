c135bb661a8aff0dcc282e7ddf1d71276fcc5a14
Joy <parkerjn90@gmail.com>
2022-04-15 16:06:40 -0400

    pass all stack and queue tests

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,5 +1,8 @@
 var Queue = function() {
-  var queueObject = {};
+  var queueObject = {
+    storageLength: 0,
+    keysMade: 0
+  };
   _.extend(queueObject, queueMethods);
   return queueObject;
 };
@@ -8,28 +11,28 @@ var queueMethods = {};
 // create storage object
 var storage = {};
 // create storageLength counter
-var storageLength = 0;
+// var storageLength = 0;
 // create counter variable to store number of Keys made
-var keysMade = 0;
+// var keysMade = 0;
 // add key (enqueue) to queueMethods, value = function with input parameter = value
 queueMethods.enqueue = function(value) {
   // add key (current storageLength + keysMade) to storage, value = input value, increment storageLength by 1
-  storage[storageLength + keysMade] = value;
-  storageLength++;
+  storage[this.storageLength + this.keysMade] = value;
+  this.storageLength++;
 };
 
 // add key (dequeue) to queueMethods, value = function
 queueMethods.dequeue = function() {
   // save value of key meant to be removed (key = storage length + keysMade)
-  var saveValue = storage[keysMade];
+  var saveValue = storage[this.keysMade];
   // remove property at storage(length + keysMade)
-  delete (storage[keysMade]);
+  delete (storage[this.keysMade]);
   // increase keysMade +1
-  keysMade++;
+  this.keysMade++;
   // if storageLength > 0,
-  if (storageLength > 0) {
+  if (this.storageLength > 0) {
     // decrease storage Length - 1
-    storageLength--;
+    this.storageLength -= 1;
   }
   // return saved value
   return saveValue;
@@ -37,5 +40,5 @@ queueMethods.dequeue = function() {
 
 // store function to indicate size at size key in queueMethods
 queueMethods.size = function() {
-  return storageLength;
+  return this.storageLength;
 };
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,6 +1,8 @@
 var Stack = function () {
   // create object that holds methods (shared by all instances of the class) and that will be returned so that the keys inside can be accessed
-  var newObject = {};
+  var newObject = {
+    stackLength: 0
+  };
   // use _.extend to copy methods onto the instance
   _.extend(newObject, stackMethods);
   // return new object
@@ -15,25 +17,31 @@ var stackMethods = {};
 // create a new stack
 var storage = {};
 
-// new stackLength variable set to zero
-var stackLength = 0;
-
+// // new stackLength variable set to zero
+// var stackLength = 0; // 1, 2,
+// var newStack2 = Stack();
+// newStack2.push('value2');
 stackMethods.push = function(value) {
-  storage[stackLength] = value;
-  stackLength++;
+  storage[this.stackLength] = value;
+
+  this.stackLength++;
 };
 
 stackMethods.pop = function() {
-  var saveValue = storage[stackLength - 1];
-  delete storage[stackLength - 1];
-  if (stackLength > 0) {
-    stackLength--;
+  var saveValue = storage[this.stackLength - 1]; // stackLength = 2
+  delete storage[this.stackLength - 1];
+  if (this.stackLength > 0) {
+    this.stackLength--;
   }
   return saveValue;
 };
 
 // add size key (value set to stackLength) to stackMethods
 stackMethods.size = function() {
-  return stackLength;
+  return this.stackLength;
 };
+//construct a newStack via the constructor
+//add an item to newStack
+
+
 
