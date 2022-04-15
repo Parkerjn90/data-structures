1d7b1072bf1ef21bca447786c950c6424f3aaf2b
Joy <parkerjn90@gmail.com>
2022-04-15 08:21:57 -0400

    pass test - new stack size = 0

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,29 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Stack = function () {
+  // create object that holds methods (shared by all instances of the class) and that will be returned so that the keys inside can be accessed
+  var newObject = {};
+
+
+  // use _.extend to copy methods onto the instance
+
+  _.extend(newObject, stackMethods);
+  // return new object
+  return newObject;
 };
 
 var stackMethods = {};
+
+// use keyword "this"
+// do not use keyword "new" or "prototype chains"
+
+
+// create a new stack
+var storage = {};
+
+// new stackLength variable set to zero
+var stackLength = 0;
+// add size key (value set to stackLength) to stackMethods
+stackMethods.size = function () {
+  return stackLength;
+};
 
 
