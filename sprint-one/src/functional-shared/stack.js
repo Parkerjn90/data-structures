8c5569f885784009440926761d5e5aa880a9b645
Joy <parkerjn90@gmail.com>
2022-04-15 08:46:38 -0400

    pass stack tests except two (size 1 after +2 -1) (size 0 after - more than exist)

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,10 +1,7 @@
 var Stack = function () {
   // create object that holds methods (shared by all instances of the class) and that will be returned so that the keys inside can be accessed
   var newObject = {};
-
-
   // use _.extend to copy methods onto the instance
-
   _.extend(newObject, stackMethods);
   // return new object
   return newObject;
@@ -15,15 +12,28 @@ var stackMethods = {};
 // use keyword "this"
 // do not use keyword "new" or "prototype chains"
 
-
 // create a new stack
 var storage = {};
 
 // new stackLength variable set to zero
 var stackLength = 0;
-// add size key (value set to stackLength) to stackMethods
-stackMethods.size = function () {
-  return stackLength;
+
+stackMethods.push = function(value) {
+  storage[stackLength] = value;
+  stackLength++;
 };
 
+stackMethods.pop = function() {
+  var saveValue = storage[stackLength - 1];
+  delete storage[stackLength - 1];
+  if (stackLength > 0) {
+    stackLength--;
+  }
+  return saveValue;
+};
+
+// add size key (value set to stackLength) to stackMethods
+stackMethods.size = function() {
+  return stackLength;
+};
 
