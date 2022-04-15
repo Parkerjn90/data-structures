114a3b2d13f8ac61ba105f172ec301cc53d08df7
Joy <parkerjn90@gmail.com>
2022-04-15 17:40:27 -0400

    pass all queue tests

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,36 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.stackLength = 0;
+  this.keysMade = 0;
+  this.storage = {};
+};
+
+//co = current object
+
+Queue.prototype.enqueue = function(value) {
+  // add inputvalue to storage(co), key = storage(co) + keysmade(co)
+  this.storage[this.stackLength + this.keysMade] = value;
+  // increase storage(co)
+  this.stackLength++;
+};
+
+Queue.prototype.dequeue = function() {
+  // save oldest value in stack
+  var oldestKey = this.storage[this.keysMade];
+  // remove oldest property in stack
+  delete this.storage[this.keysMade];
+  // increase keys made +1
+  this.keysMade++;
+  // if current stack Length > 0
+  if (this.stackLength > 0) {
+    // decrease stack length
+    this.stackLength++;
+  }
+  // return oldest value in stack
+  return oldestKey;
+};
+
+Queue.prototype.size = function() {
+  return this.stackLength;
 };
 
 
