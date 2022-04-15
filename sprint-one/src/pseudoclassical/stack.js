edde5f956b25d036ac24aa259dba874ae7e642d1
Joy <parkerjn90@gmail.com>
2022-04-15 17:01:25 -0400

    pass all stack tests

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.stackLength = 0;
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+  // assign value to current object, key = current length AND increase current length +1
+  this.storage[this.stackLength++] = value;
+};
 
+Stack.prototype.pop = function() {
+  // save most recent value (key = current obj length - 1)
+  var recentValue = this.storage[this.stackLength - 1];
+  // remove most recent property
+  delete this.storage[this.stackLength - 1];
+  // as long as length is > 0
+  if (this.stackLength > 0) {
+    // decrease length
+    this.stackLength--;
+  }
+  return recentValue;
+};
+
+Stack.prototype.size = function() {
+  return this.stackLength;
+};
+
+// var newStack = new Stack();
