feba6ab7839ca92bc2936410447a0eab2eec5ee9
Joy <parkerjn90@gmail.com>
2022-04-15 16:47:36 -0400

    pass all of queue and stack tests

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,35 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(queueMethods);
+  newStack.stackLength = 0;
+  newStack.keysMade = 0;
+  newStack.storage = {};
+  return newStack;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  // add value to storage(current obj), key = stackLength(current obj) AND
+  this.storage[this.stackLength + this.keysMade] = value;
+  //increase stackLength
+  this.stackLength++;
+};
 
+queueMethods.dequeue = function() {
+  // save value of oldest added property
+  var recentValue = this.storage[this.keysMade];
+  // remove oldest added property
+  delete this.storage[this.keysMade];
+  // increase keys made +1
+  this.keysMade++;
+  //as long as stackLength is above 0
+  if (this.stackLength > 0) {
+    // decrease stackLength
+    this.stackLength--;
+  }
+  return recentValue;
+};
+
+queueMethods.size = function() {
+  return this.stackLength;
+};
