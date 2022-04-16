a68c1bcb7968a26044b06f8511df7a9a747b688f
Joy <parkerjn90@gmail.com>
2022-04-16 10:27:46 -0400

    pass all tree tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,54 @@
-var Tree = function(value) {
+var Tree = function (value) {
   var newTree = {};
   newTree.value = value;
-
+  // addChild: this.children.push({value: value})
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; // [{5: 5}]
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value) {
+treeMethods.addChild = function (value) {
+  // push current value
+  this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function (target) {
+  var target = target;
+  var result = false;
+
+  if (this.value === target) {
+    result = true;
+  } else {
+    // check for children... if yes...
+    if (this.children.length) {
+      // for EACH child,
+      for (var i = 0; i < this.children.length; i++) {
+        console.log('this.children[i]', this.children[i]);
+        // result will equal whether or not each(any) child contains target
+        if (this.children[i].contains(target)) {
+          result = true;
+        }
+      }
+    }
+  }
+  // return result
+  return result;
 };
+
+// var newTree = Tree('x')
+// newTree.value
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+// I - value
+// O - an object with the properties:
+         // value: = inputvalue
+         // children: =
