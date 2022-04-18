7c55941ec29110827b9b6ba1428ee7adb7f1993f
Joy <parkerjn90@gmail.com>
2022-04-18 08:19:05 -0400

    build constructor function and add required methods

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,31 @@
-var BinarySearchTree = function(value) {
+var BinarySearchTree = function (value) {
+  // link newTree with treeMethods
+  var newTree = Object.create(treeMethods);
+  // add value property
+  this.value = value;
+  // add left node (empty right now)
+  this.left = null;
+  // add right node (empty right now)
+  this.right = null;
+  // return newTree
+  return newTree;
 };
 
+var treeMethods = {};
+
+treeMethods.insert = function (value) {
+
+};
+
+treeMethods.contains = function (value) {
+
+};
+
+treeMethods.depthFirstLog = function (callback) {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+console.log(treeMethods);
